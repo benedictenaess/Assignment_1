2beea9a54141b20ee3ef9e4599cc654de19c918f
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2024-02-01 16:00:32 +0100

    Print function

--- js/app.js
+++ js/app.js
@@ -37,3 +37,31 @@ if(vehicleButton){
 		window.open('/html/vehicle.html', '_blank');
 	})
 }
+
+//PRINT FUNCTION
+function printInfo (img, first, second, third, fourth, fifth, targetElement) {
+	//creating elements
+	const mainList = document.createElement('li');
+	const image = document.createElement('img');
+	const item1 = document.createElement('div');
+	const item2 = document.createElement('div');
+	const item3 = document.createElement('div');
+	const item4 = document.createElement('div');
+	const item5 = document.createElement('div');
+
+	//classes
+	mainList.classList.add('main-cards');
+	image.classList.add('properties-img');
+
+	//appending
+	mainList.append(image, item1, item2, item3, item4, item5);
+	targetElement.append(mainList);
+
+	//content
+	image.src = img;
+	item1.textContent = first;
+	item2.textContent = second;
+	item3.textContent = third;
+	item4.textContent = fourth;
+	item5.textContent = fifth;
+}
